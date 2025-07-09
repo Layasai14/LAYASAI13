4ecc9112521620d7102a8f36c0f97e529b0d7c5c
Layasai14 <anthanagarilaya@gmail.com>
2025-07-09 14:09:24 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,19 +1,17 @@
 import joblib
-import numpy as np
+import pandas as pd
 
-df = pd.read_csv('iris.data.csv', header=None)
-df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-
-
-# Load model and label encoder
+# Load model
 model = joblib.load('knn_model.pkl')
-label_encoder = joblib.load('label_encoder.pkl')
 
-# Example input: sepal_length, sepal_width, petal_length, petal_width
-sample_input = np.array([[5.1, 3.5, 1.4, 0.2]])
+# Example input data
+sample = pd.DataFrame({
+    'sepal_length': [5.1],
+    'sepal_width': [3.5],
+    'petal_length': [1.4],
+    'petal_width': [0.2]
+})
 
 # Predict
-predicted_class = model.predict(sample_input)[0]
-predicted_label = label_encoder.inverse_transform([predicted_class])[0]
-
-print(f"Predicted Iris class: {predicted_label}")
+prediction = model.predict(sample)
+print("Predicted species:", prediction[0])
